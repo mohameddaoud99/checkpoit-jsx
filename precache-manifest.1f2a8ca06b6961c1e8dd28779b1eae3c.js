414027b66731685e91c8ecfb4ede761d4bce6910
mohameddaoud99 <mohamed.daoud99@hotmail.com>
2020-07-14 11:52:30 +0200

    Updates

---
+++ precache-manifest.1f2a8ca06b6961c1e8dd28779b1eae3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc186539a82f47d5a53a588f356b3e87",
+    "url": "/checkpoit-jsx/index.html"
+  },
+  {
+    "revision": "ae3203e7e16a0ba7342d",
+    "url": "/checkpoit-jsx/static/css/main.6be58eea.chunk.css"
+  },
+  {
+    "revision": "26e679ac0ee9be84be3d",
+    "url": "/checkpoit-jsx/static/js/2.89a9fd65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/checkpoit-jsx/static/js/2.89a9fd65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3203e7e16a0ba7342d",
+    "url": "/checkpoit-jsx/static/js/main.adfbe50e.chunk.js"
+  },
+  {
+    "revision": "f78c085c7bd2e95d1cd8",
+    "url": "/checkpoit-jsx/static/js/runtime-main.8693e856.js"
+  },
+  {
+    "revision": "a770d88c05989eb75145428a36b04eca",
+    "url": "/checkpoit-jsx/static/media/image2.a770d88c.png"
+  }
+]);
